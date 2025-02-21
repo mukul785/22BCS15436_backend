44b22d381fd3869dbe5e7c30b33d1cfade0ce598
mukul785 <156766802+mukul785@users.noreply.github.com>
2025-02-21 19:02:17 +0530

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(json());
 const corsOptions = {
-    origin: 'https://22bcs15436-front.vercel.app',
+    origin: 'https://22bcs15436api.netlify.app',
     methods: ['GET', 'POST'], 
     allowedHeaders: ['Content-Type'], 
 };
