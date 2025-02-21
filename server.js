35b5ebe2e7c16e2aed1b0317dca65ed8ac83cd47
mukul785 <156766802+mukul785@users.noreply.github.com>
2025-02-21 18:00:09 +0530

    commit

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(json());
-app.use(cors());
+app.use(cors({
+    origin: ['https://22bcs15436-front.vercel.app/'], 
+    methods: ['GET', 'POST'],
+}));
 
 app.post('/bfhl', (req, res) => {
     const inputArray = req.body.data;
